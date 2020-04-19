ff5842425571794340b49e08f26c62d92966c574
Pavel <pashamyl999@gmail.com>
2020-04-19 18:34:19 +0600

    Lesson 6 code. Доработанный код.

--- src/lesson6/ClientClass.java
+++ src/lesson6/ClientClass.java
@@ -16,72 +16,66 @@ public class ClientClass {
     private DataOutputStream dataOutputStream;
     private DataInputStream dataInputStream;
 
-    public ClientClass() {
-        initServerConnection();
+    public ClientClass() throws IOException {
+        processMessagesFromServer();
     }
 
-    private void initServerConnection() {
 
+    private void processMessagesFromServer() throws IOException {
+        Socket socket = null;
+        Thread thread = null;
         try {
-
-            Socket socket = new Socket();
+            socket = new Socket();
             socket.connect(new InetSocketAddress("localhost", SERVER_PORT), CONNECTION_TIMEOUT);
             dataInputStream = new DataInputStream(socket.getInputStream());
             dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            System.out.println("Соединение установлено!");
 
+            thread = createInputThread();
+            runOutputLoop(dataOutputStream);
 
-            processMessagesFromServer(socket);
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private void processMessagesFromServer(Socket socket)  {
-        System.out.println("Соединение установлено!");
-        System.out.println("Введите сообщение:\n");
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    while (true) {
-
-                        String message = dataInputStream.readUTF();
-                        if ("/end".equals(message)) {
-                            System.out.println("Соединение с сервером было закрыто!\n");
-                            break;
-                        }
-                        System.out.println("Server - " + message);
-                    }
-
-                } catch (IOException ex) {
-                    System.out.println("Ситуация 1");
-                    System.out.println("Соединение было закрыто!");
-                }
-            }
-
-        }).start();
-        Scanner sc = new Scanner(System.in);
-
-        try {
-            while (true) {
-                String message = sc.nextLine();
-                if (message.equals("/end")) {
-                    socket.close();
-                    sc.close();
-                    break;
-                }
-                dataOutputStream.writeUTF(message);
-            }
         } catch (IOException e) {
             System.out.println("Ситуация 2");
             System.out.println("Соединение было закрыто!");
+        } finally {
+            if (thread != null) createInputThread().interrupt();
+            if (socket != null) socket.close();
         }
 
     }
 
+    private void runOutputLoop(DataOutputStream out) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String message = sc.next();
+            out.writeUTF(message);
+            if (message.equals("/end")) {
+                break;
+            }
+        }
+    }
 
-    public static void main(String[] args) {
+    private Thread createInputThread() {
+        Thread thread = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String message = dataInputStream.readUTF();
+                    System.out.println("Server - " + message);
+                    if ("/end".equals(message)) {
+                        System.exit(0);
+                    }
+                } catch (IOException ex) {
+                    System.out.println("Соединение было закрыто!");
+                    break;
+                }
+            }
+        });
+        thread.start();
+        return thread;
+    }
+
+
+    public static void main(String[] args) throws IOException {
         ClientClass clientClass = new ClientClass();
     }
 
--- src/lesson6/ServerClass.java
+++ src/lesson6/ServerClass.java
@@ -13,66 +13,71 @@ public class ServerClass {
 
     public static final int SERVER_PORT = 8189;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        new ServerClass().start();
+    }
+
+    private void start() throws IOException {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        Thread thread = null;
         System.out.println("Старт эхо-сервера");
         try {
-            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            serverSocket = new ServerSocket(SERVER_PORT);
             System.out.println("Сервер запущен, ожидаем подключения...");
-            Socket clientSocket = serverSocket.accept();
+            clientSocket = serverSocket.accept();
             System.out.println("Клиент подключился!");
 
             DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
             DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
 
-            Scanner sc = new Scanner(System.in);
-            System.out.println("\nВы можете писать сообщения клиенту тут:\n");
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
 
-                        System.out.println("Запускаю поток");
-                        while (true) {
-                            //if (){
-                                System.out.println("Захожу в цикл");
-                                String messageToClient = sc.nextLine();
-                                if ("/end".equals(messageToClient)) {
-                                    sc.close();
-                                    break;
-                                }else if(!messageToClient.isEmpty()){
-                                dataOutputStream.writeUTF(messageToClient);
-                                }
-                            //}
-                        }
-                        System.out.println("Вышел из цикла!");
-                    } catch (IOException e) {
-                        System.out.println("Cитуация 1");
-                        e.getMessage();
-                    }
-                }
-            }).start();
+            thread = createInputThread(dataInputStream);
+            createOutputLoop(dataOutputStream);
 
-            try {
-                while (true) {
-                    String messageFromClient = dataInputStream.readUTF();
-                    System.out.println("From client: " + messageFromClient);
-                    if ("/end".equals(messageFromClient)) {
-                        sc.close();
-                        break;
-                    }
-                    dataOutputStream.writeUTF("Эхо - " + messageFromClient);
-                }
-            } catch (IOException e) {
-                System.out.println("Cитуация 2");
-                e.getMessage();
-            }
 
             serverSocket.close();
-
         } catch (IOException e) {
             System.err.println("Port " + SERVER_PORT + " is already used");
             e.printStackTrace();
+        } finally {
+            if (thread != null) thread.interrupt();
+            if (clientSocket != null) clientSocket.close();
+            if (serverSocket != null) serverSocket.close();
         }
+    }
 
+    private static void createOutputLoop(DataOutputStream dataOutputStream) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String messageToClient = sc.next();
+            dataOutputStream.writeUTF(messageToClient);
+            if ("/end".equals(messageToClient)) {
+                System.exit(0);
+                break;
+            }
+        }
+    }
+
+    private static Thread createInputThread(DataInputStream dataInputStream) {
+
+        Thread thread = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String messageFromClient = dataInputStream.readUTF();
+                    System.out.println("From client: " + messageFromClient);
+                    if ("/end".equals(messageFromClient)) {
+                        System.exit(0);
+                        break;
+                    }
+                } catch (IOException e) {
+                    System.out.println("Connection was closed");
+                    break;
+                }
+            }
+
+        });
+        thread.start();
+        return thread;
     }
 }
